b50c5c059b94865aabe5dbef83064bae79b8796e
Sang Nguyen <ngcongsang@gmail.com>
2020-10-28 23:26:15 +0700

    update video url path

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -5,7 +5,7 @@ import addContext from 'mochawesome/addContext';
 Cypress.on('test:after:run', (test, runnable) => {
   let videoName = Cypress.spec.name;
   videoName = videoName.replace('/.ts.*', '.ts');
-  const videoUrl = 'cypress/videos/' + videoName + '.mp4';
+  const videoUrl = 'videos/' + videoName + '.mp4';
 
   addContext({test}, videoUrl);
 });
